5adc308c5b85f97fb92118dc27da79b65f34c5b6
Alex Fila <filal41sng@gmail.com>
2021-07-08 10:19:43 +0300

    rebuild index func in views

--- navi/nbi_navi/views.py
+++ navi/nbi_navi/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News #чтоб достать данные из News
+
 
 def index(request):
-    print(request)
-    return HttpResponse('hi there')
+    news = News.objects.all()
+    res = '<h1>Список новостей</h1>\n'
+    for new in news:
+        res += f'<div>\n<p>{new.title}</p>\n<p>{new.content}</p>\n</div>\n<hr>'
+    return HttpResponse(res)
 
 def test(request):
     return HttpResponse('<h1>Тестовая страница</h1>')
