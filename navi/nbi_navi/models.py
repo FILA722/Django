10f90e8f4c7f395ffba51b10a4b4f1e817958164
Alex Fila <filal41sng@gmail.com>
2021-07-08 16:26:09 +0300

    new class Category added

--- navi/nbi_navi/models.py
+++ navi/nbi_navi/models.py
@@ -7,6 +7,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, default=1)
 
     def __str__(self):
         return self.title
@@ -18,3 +19,14 @@ class News(models.Model):
         verbose_name = 'Новость' #Единичное склонение
         verbose_name_plural = 'Новости' #Множественное склонение
         ordering = ['-created_at', 'title'] #Порядок отображения
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+
+    class Meta:
+        """
+        Этот класс отвечает за отображение полей на странице
+        """
+        verbose_name = 'Категория' #Единичное склонение
+        verbose_name_plural = 'Категории' #Множественное склонение
+        ordering = ['title'] #Порядок отображения
