273ed899debc88270407c7a4635657b364a77377
Alex Fila <filal41sng@gmail.com>
2021-07-08 15:33:55 +0300

    admin_page rebuilded

--- navi/nbi_navi/apps.py
+++ navi/nbi_navi/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class NbiNaviConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'nbi_navi'
+    verbose_name = 'NBI Navigator'
--- navi/nbi_navi/views.py
+++ navi/nbi_navi/views.py
@@ -4,5 +4,6 @@ from .models import News #чтоб достать данные из News
 
 
 def index(request):
-    news = News.objects.all()
-    return render(request, 'nbi_navi/index.html', {'news': news, 'title': 'Список новостей'})  #request, temple_name, DATA
+    news = News.objects.all
+    data = {'news': news, 'title': 'Список новостей'}
+    return render(request, 'nbi_navi/index.html', data)  #request, temple_name, DATA
