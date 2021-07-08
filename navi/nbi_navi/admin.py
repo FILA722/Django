6ee316d2855efd036d9288937ac659ac5c6862fb
Alex Fila <filal41sng@gmail.com>
2021-07-08 16:56:29 +0300

    added filter and editer to admin panel

--- navi/nbi_navi/admin.py
+++ navi/nbi_navi/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Category
 
 class NewsAdmin(admin.ModelAdmin):
     """
     Этот класс отвечает за отображение нужных полей из БД в админке
     """
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',) #чтоб менять из админки
+    list_filter = ('is_published', 'category') #добавить в админку поле с функцией фильтрации
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Этот класс отвечает за отображение нужных полей из БД в админке
+    """
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
 # Register your models here.
 admin.site.register(News, NewsAdmin) #To Registry model News and NewsAdmin
+admin.site.register(Category, CategoryAdmin)
