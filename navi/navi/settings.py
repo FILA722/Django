f0a83e40ee5b6a712217bd82505aede4ce3b918d
Alex Fila <filal41sng@gmail.com>
2021-07-15 12:01:36 +0300

    entered full path to the NbiNaviConfig class

--- navi/navi/settings.py
+++ navi/navi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nbi_navi'
+    'nbi_navi.apps.NbiNaviConfig'
 ]
 
 MIDDLEWARE = [
