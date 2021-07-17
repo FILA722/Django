7a2823885925cbfba8f4f52426d2b02bc89857b4
Alex Fila <filal41sng@gmail.com>
2021-07-17 12:49:13 +0300

    added category page

--- navi/nbi_navi/urls.py
+++ navi/nbi_navi/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('category/<int:category_id>/', get_category) # category/{1,2,3,4...} принимает id категории
 ]
--- navi/nbi_navi/views.py
+++ navi/nbi_navi/views.py
@@ -4,7 +4,13 @@ from .models import News, Category #чтоб достать данные из Ne
 
 
 def index(request):
-    news = News.objects.all
-    categories = Category.objects.all
-    data = {'news': news, 'title': 'Список новостей', 'category': categories}
+    news = News.objects.all()
+    categories = Category.objects.all()
+    data = {'news': news, 'title': 'Список новостей', 'categories': categories}
     return render(request, 'nbi_navi/index.html', data)  #request, temple_name, DATA
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'nbi_navi/category.html', {'news': news, 'categories': categories, 'category': category})
