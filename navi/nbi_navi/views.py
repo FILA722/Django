2c1405021f8a723824c81fa9cc006f6e2a18c9bb
Alex Fila <filal41sng@gmail.com>
2021-07-17 12:05:22 +0300

    added category field

--- navi/nbi_navi/views.py
+++ navi/nbi_navi/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News #чтоб достать данные из News
+from .models import News, Category #чтоб достать данные из News
 
 
 def index(request):
     news = News.objects.all
-    data = {'news': news, 'title': 'Список новостей'}
+    categories = Category.objects.all
+    data = {'news': news, 'title': 'Список новостей', 'category': categories}
     return render(request, 'nbi_navi/index.html', data)  #request, temple_name, DATA
