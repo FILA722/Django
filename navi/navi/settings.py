451902f052810b4d5e084221e3f8c2543a1cec67
Alex Fila <filal41sng@gmail.com>
2021-07-18 15:07:11 +0300

    added _nav.html with Nav bar

--- navi/navi/settings.py
+++ navi/navi/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'navi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], #добавил новую директорию для поиска шаблона base.html
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
