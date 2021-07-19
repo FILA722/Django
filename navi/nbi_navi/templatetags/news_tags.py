67c333603776357352b8c58d55857b1606f8e53c
Alex Fila <filal41sng@gmail.com>
2021-07-20 00:44:30 +0300

    created custom inclusion tag

--- navi/nbi_navi/templatetags/news_tags.py
+++ navi/nbi_navi/templatetags/news_tags.py
@@ -3,6 +3,11 @@ from nbi_navi.models import Category
 
 register = template.Library()  #регистрация TemplateTag
 
-@register.simple_tag()
+@register.simple_tag(name='get_list_categories')
 def get_categories():
     return Category.objects.all()
+
+@register.inclusion_tag('nbi_navi/list_categories.html')
+def show_categories():
+    categories = Category.objects.all()
+    return {'categories': categories}
