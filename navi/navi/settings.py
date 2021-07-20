febbe3b3c7b418777093e14c734574e22285a086
Alex Fila <filal41sng@gmail.com>
2021-07-20 17:10:40 +0300

    static collected & base.html rebuild with static js/css routes

--- navi/navi/settings.py
+++ navi/navi/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'navi/static'),]
 
 #Папка, в которую нужно загужать файлы
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
