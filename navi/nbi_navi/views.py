81fd2777e03e4eec8d52c309f6f2b13ffa6b8aab
Alex Fila <filal41sng@gmail.com>
2021-07-23 15:34:31 +0300

    saved input object added

--- navi/nbi_navi/views.py
+++ navi/nbi_navi/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import News, Category #чтоб достать данные из News
 from .forms import NewsForm
@@ -24,7 +24,10 @@ def navi(request):
 
 def add_news(request):
     if request.method == "POST":
-        pass
+        form = NewsForm(request.POST) #забрать данные с формы
+        if form.is_valid():
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'nbi_navi/add_news.html', {'form': form})
