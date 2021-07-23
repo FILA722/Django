02de3212bcc0e9a1a5562532a9f9550c9659a45f
Alex Fila <filal41sng@gmail.com>
2021-07-23 15:08:32 +0300

    add_news form edited

--- navi/nbi_navi/forms.py
+++ navi/nbi_navi/forms.py
@@ -3,7 +3,7 @@ from .models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название ')
-    content = forms.CharField(label='Новость ', required=False)
+    title = forms.CharField(max_length=150, label='Название ', widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label='Новость ', required=False, widget=forms.Textarea(attrs={"class": "form-control", "rows": 5}))
     is_published = forms.BooleanField(label="Опубликовано ", required=False, initial=True)
-    category = forms.ModelChoiceField(empty_label="Выберите категорию", queryset=Category.objects.all(), label="Категория ")
+    category = forms.ModelChoiceField(empty_label="Выберите категорию", queryset=Category.objects.all(), label="Категория ", widget=forms.Select(attrs={"class": "form-control"}))
